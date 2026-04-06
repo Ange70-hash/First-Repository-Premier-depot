ae1abfef1c3eee6a11db15f3face6d545633fe25
Ange <ange6398@outlook.com>
2026-04-06 16:06:55 -0400

    feat: change password

--- loginfile.py
+++ loginfile.py
@@ -1,7 +1,7 @@
 username = input("non d'utilisateur : ")
 password = input("mot de passe : ")
 
-if username == "admin" and password == "1234":
+if username == "admin" and password == "abc":
     print("accès autorisé")
 else:
     print("accès refusé")
